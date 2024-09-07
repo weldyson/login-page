85e9b598aedf54f5ffa82ceda4f88a617f1cf12a
Azevedo <weldysona@gmail.com>
2024-09-07 17:36:32 -0300

    Signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './pages/signup/login.component';
 
 
 export const routes: Routes = [
