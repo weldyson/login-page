fb4c53d43aa010f48243c4045a63d4045eb67c40
Azevedo <weldysona@gmail.com>
2024-09-07 17:48:17 -0300

    Signup fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/signup/login.component';
-
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
     {
         path: "login",
         component: LoginComponent
+    },
+    {
+        path: "signup",
+        component: SignUpComponent
     }
 ];
