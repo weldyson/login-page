d4b1baacef52ee65bd83ec9097ac3d99656b3d5d
Azevedo <weldysona@gmail.com>
2024-09-07 14:33:26 -0300

    Style

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    DefaultLoginLayoutComponent
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!: FormGroup
+
+  constructor(){
+    this.loginForm = new FormGroup({
+      email: new FormGroup('', [Validators.required, Validators.email]),
+      password: new FormGroup('', [Validators.required, Validators.minLength(6)])
+
+    })
+
+  }
 
 }
